a4d026b0bff594b0411e9aa32c0fbe9e40a73052
spro3008 <spro3008@7-11.com>
2023-11-30 16:30:01 +0100

    added appyx navigation for windows

--- composeApp/src/commonMain/kotlin/pokemonList/presentation/PokemonListNode.kt
+++ composeApp/src/commonMain/kotlin/pokemonList/presentation/PokemonListNode.kt
@@ -1,23 +1,44 @@
 package pokemonList.presentation
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.bumble.appyx.components.backstack.BackStack
+import com.bumble.appyx.components.backstack.BackStackModel
 import com.bumble.appyx.components.backstack.operation.pop
 import com.bumble.appyx.components.backstack.operation.push
+import com.bumble.appyx.components.backstack.ui.fader.BackStackFader
+import com.bumble.appyx.navigation.composable.AppyxComponent
 import com.bumble.appyx.navigation.modality.BuildContext
 import com.bumble.appyx.navigation.node.Node
 import com.bumble.appyx.navigation.node.ParentNode
-import core.configs.NavTarget
+import com.bumble.appyx.navigation.node.node
+import com.bumble.appyx.utils.multiplatform.Parcelable
+import com.bumble.appyx.utils.multiplatform.Parcelize
 import pokemonDetails.presentation.PokemonDetailsNode
 
 class PokemonListNode(
     buildContext: BuildContext,
-    private val backStack: BackStack<NavTarget>,
-) : ParentNode<NavTarget>(
+    private val backStack: BackStack<NavTarget> = BackStack(
+        model = BackStackModel(
+            initialTarget = NavTarget.PokemonList,
+            savedStateMap = buildContext.savedStateMap,
+        ),
+        visualisation = { BackStackFader(it) },
+    ),
+) : ParentNode<PokemonListNode.NavTarget>(
     buildContext = buildContext,
     appyxComponent = backStack,
 ) {
+
+    sealed class NavTarget : Parcelable {
+        @Parcelize
+        data class PokemonDetails(val pokemonId: Int) : NavTarget()
+
+        @Parcelize
+        data object PokemonList : NavTarget()
+    }
+
     override fun resolve(interactionTarget: NavTarget, buildContext: BuildContext): Node =
         when (interactionTarget) {
             is NavTarget.PokemonDetails -> PokemonDetailsNode(
@@ -25,15 +46,19 @@ class PokemonListNode(
                 buildContext = buildContext,
                 navigateBack = { backStack.pop() },
             )
+
+            is NavTarget.PokemonList -> node(buildContext = buildContext) {
+                PokemonListScreen(onItemClick = { pokemonId ->
+                    backStack.push(NavTarget.PokemonDetails(pokemonId = pokemonId))
+                })
+            }
         }
 
     @Composable
     override fun View(modifier: Modifier) {
-        PokemonListScreen(
-            modifier = modifier,
-            onItemClick = { pokemonId: Int ->
-                backStack.push(NavTarget.PokemonDetails(pokemonId = pokemonId))
-            },
+        AppyxComponent(
+            modifier = Modifier.fillMaxSize(),
+            appyxComponent = backStack,
         )
     }
 }
--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,14 +1,28 @@
+import androidx.compose.runtime.remember
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.bumble.appyx.navigation.integration.DesktopNodeHost
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+sealed class Events {
+    object OnBackPressed : Events()
+}
 
 fun main() = application {
     val events: Channel<Events> = Channel()
@@ -16,6 +30,7 @@ fun main() = application {
     val eventScope = remember { CoroutineScope(SupervisorJob() + Dispatchers.Main) }
 
     Window(
+        title = "Pokemons",
         state = windowState,
         onCloseRequest = ::exitApplication,
         onKeyEvent = {
@@ -39,3 +54,16 @@ fun main() = application {
     }
 }
 
+private fun onKeyEvent(
+    keyEvent: KeyEvent,
+    events: Channel<Events>,
+    coroutineScope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main),
+): Boolean =
+    when {
+        keyEvent.type == KeyEventType.KeyDown && keyEvent.key == Key.Backspace -> {
+            coroutineScope.launch { events.send(Events.OnBackPressed) }
+            true
+        }
+
+        else -> false
+    }
